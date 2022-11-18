ac2c0812c510ee4dd97483401fc8e14cc75af161
amritap-ef <amrita.panesar@ef.com>
2022-11-18 09:36:41 +0000

    Add es index search function for captions

--- content_recomm/es_recomm_content.py
+++ content_recomm/es_recomm_content.py
@@ -2,6 +2,7 @@ import os
 import json
 from elasticsearch import Elasticsearch
 
+INDEX_NAME = "test"
 def _get_elasticsearch_url():
     curr_dir = os.path.dirname(os.path.realpath(__file__))
     elasticsearch_fpath = os.path.join(curr_dir, "..", "secrets/elasticsearch.json")
@@ -11,29 +12,37 @@ def _get_elasticsearch_url():
     return es_url
 
 
-def recomm_content(phrase, cefr_level, content_type: list, es_url):
+def index_exact_search_relevance_metadata(phrase, cefr_level, content_type: list, es_url):
     es = Elasticsearch(
         es_url,
         max_retries=10
     )
-    INDEX_NAME = "test"
+
     body = {
       "_source": [
         "metadata.headline",
         "metadata.url",
         "metadata.article_body_html",
         "metadata.author",
-        "cefr.pred"
-        ],
+        "cefr.pred",
+        "topic_classification.topic",
+        "metadata.type",
+        "captions.start"
+      ],
       "query": {
         "bool": {
           "must": [
             {
-            "terms": {
-              "cefr.pred": [
-                cefr_level
-              ]
-            }
+                "terms": {
+                  "metadata.type": content_type
+                }
+            },
+            {
+                "terms": {
+                  "cefr.pred": [
+                    cefr_level
+                  ]
+                }
             },
             {
               "multi_match": {
@@ -47,6 +56,74 @@ def recomm_content(phrase, cefr_level, content_type: list, es_url):
     }
     results = es.search(index=INDEX_NAME, body=body)
     results = results["hits"]["hits"][:3]
-
     return results
 
+def index_exact_search_relevance_captions(keywords: str, cefr_level: str, es_url) -> dict:
+    es = Elasticsearch(
+        es_url,
+        max_retries=10
+    )
+
+    query_exact_search = {
+        "_source": [
+            "metadata.source",
+            # "metadata.show_name",
+            "metadata.author",
+            "metadata.description",
+            "metadata.headline",
+            "metadata.date_published",
+            "metadata.publisher",
+            "metadata.url",
+            "captions.evp.tags.label",
+            "captions.evp.tags.text",
+            "captions.evp.tags.url",
+            "captions.evp.tags.text",
+            "cefr.pred",
+            "sentiment",
+            "evp",
+            "style"
+        ],
+        "size": 3,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "terms": {
+                            "metadata.type": ["video"]
+                        }
+                    },
+                    {
+                        "terms": {
+                            "cefr.pred.keyword": [cefr_level]
+                        }
+                    },
+                    {
+                        "nested": {
+                            "path": "captions",
+                            "score_mode": "avg",
+                            "query": {
+                                "multi_match": {
+                                    "query": keywords,
+                                    "type": "phrase",
+                                    "fields": ["captions.text"]
+                                }
+                            },
+                            "inner_hits": {
+                                "highlight": {
+                                    "pre_tags": ["<mark>"],
+                                    "post_tags": ["</mark>"],
+                                    "fields": {
+                                        "captions.text": {}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    results = es.search(index=INDEX_NAME, body=query_exact_search)
+    results = results["hits"]["hits"]
+    return results
